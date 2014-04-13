8de15c19de1a7aa9f702890eea103f3c238bc575
barryhughes <barry@codingkills.me>
2014-04-13 07:16:36 -0700

    Adapted the loader code (in orbit.php)

--- inc/orbit.php
+++ inc/orbit.php
@@ -1,7 +1,20 @@
 <?php
-defined( 'EVENT_ROCKET_INC' ) or die();
+defined( 'EVENTROCKET_INC' ) or die();
 
-$includes = array( 'nosecone', 'shortcodes', 'gps', '404_laser' );
+/**
+ * Event Rocket is essentially a collection of small modules that have little
+ * relationship between one another.
+ *
+ * The eventrocket_components filter provides a means of selectively deactivating
+ * modules that are unnecessary or unwanted in a specific case.
+ *
+ * @todo provide a settings page to make this process easier
+ */
+$includes = array( 'nosecone', 'shortcodes', 'gps', '404_laser', 'hud' );
+$includes = apply_filters( 'eventrocket_components', $includes );
 
-foreach ( $includes as $component )
-	require EVENT_ROCKET_INC . "/$component.php";
+foreach ( $includes as $component ) {
+	$path = EVENTROCKET_INC . "/$component.php";
+	if ( 0 !== strpos( realpath( $path), EVENTROCKET_INC ) ) continue;
+	require_once EVENTROCKET_INC . "/$component.php";
+}
