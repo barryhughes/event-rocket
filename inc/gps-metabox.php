31ae04e270b0b877155ed6772c15732166b7006c
barryhughes <barry@codingkills.me>
2014-05-07 12:01:20 -0700

    Switch GPS number inputs to text inputs to resolve issue with Chrome

--- inc/gps-metabox.php
+++ inc/gps-metabox.php
@@ -11,10 +11,10 @@ wp_nonce_field( 'event_rocket_save_long_lat', 'eventrocket_gps' );
 
 <p>
 	<label for="eventrocket_longitude"> <?php _e( 'Longitude', 'eventrocket' ); ?> </label> <br/>
-	<input type="number" name="eventrocket_longitude" id="eventrocket_longitude" value="<?php esc_attr_e( $longitude ) ?>" />
+	<input type="text" name="eventrocket_longitude" id="eventrocket_longitude" value="<?php esc_attr_e( $longitude ) ?>" />
 </p>
 
 <p>
 	<label for="eventrocket_latitude"> <?php _e( 'Latitude', 'eventrocket' ); ?> </label> <br/>
-	<input type="number" name="eventrocket_latitude" id="eventrocket_latitude" value="<?php esc_attr_e( $latitude ) ?>" />
+	<input type="text" name="eventrocket_latitude" id="eventrocket_latitude" value="<?php esc_attr_e( $latitude ) ?>" />
 </p>
