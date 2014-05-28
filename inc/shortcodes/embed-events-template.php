d6d4931aa2e8e2e1409f8915768ecb51c17277ca
barryhughes <barry@codingkills.me>
2014-05-28 12:29:22 -0700

    Link to single events from embedded event template (fixes #6)

--- inc/shortcodes/embed-events-template.php
+++ inc/shortcodes/embed-events-template.php
@@ -1,6 +1,6 @@
 <div class="eventrocket embedded-event post">
 	<div class="post-thumb"> <?php tribe_event_featured_image() ?> </div>
-	<h3> <?php the_title() ?> </h3>
+	<h3> <a href="<?php the_permalink() ?>"><?php the_title() ?></a> </h3>
 	<div class="schedule"> <?php echo tribe_events_event_schedule_details() ?> </div>
 	<div class="summary"> <?php the_excerpt() ?> </div>
 </div>
