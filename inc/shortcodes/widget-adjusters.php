587eb97b4c8e70bd1a7ce20048ce71c309d47c30
barryhughes <barry@codingkills.me>
2014-06-09 10:50:00 -0700

    Make specification of cat/tag filters by shortcode easier (fixes #7)

--- inc/shortcodes/widget-adjusters.php
+++ inc/shortcodes/widget-adjusters.php
@@ -35,6 +35,59 @@ add_filter( 'event_rocket_shortcode_tribecountdownwidget_attributes', 'eventrock
 add_filter( 'event_rocket_shortcode_tribevenuewidget_attributes', 'eventrocket_widget_id_atts', 10 );
 
 /**
+ * Support category and tag attributes when setting up calendar/list widgets.
+ *
+ * From within the shortcode, users can supply tag/category IDs or slugs using "category",
+ * "categories", "tag" and "tags" attributes. All of these are potentially valid examples:
+ *
+ *     [event_rocket_calendar category="43"]
+ *     [event_rocket_calendar category="special-events"]
+ *     [event_rocket_calendar categories="871, featured-events" tag="staff-only"]
+ *
+ * @param $atts
+ * @return mixed
+ */
+function eventrocket_tax_atts( $atts ) {
+	$filters = array();
+
+	$expect = array(
+		'categories' => '',
+		'category' => '',
+		'tags' => '',
+		'tag' => ''
+	);
+
+	$relationship = array(
+		'categories' => TribeEvents::TAXONOMY,
+		'tags' => 'post_tag'
+	);
+
+	$params = array_merge( $expect, $atts );
+	$params['categories'] = $params['categories'] . ',' . $params['category'];
+	$params['tags'] = $params['tags'] . ',' . $params['tag'];
+
+	foreach ( $relationship as $param => $tax ) {
+		$terms = explode( ',', $params[$param] );
+		foreach ( $terms as $term ) {
+			$term = trim( $term );
+			if ( empty( $term ) ) continue;
+			if ( is_numeric( $term ) && $term == absint( $term ) ) $filters[$tax][] = $term;
+			else {
+				$term_obj = get_term_by( 'slug', $term, $tax );
+				if ( false === $term_obj ) continue;
+				$filters[$tax][] = $term_obj->term_id;
+			}
+		}
+	}
+
+	if ( ! empty( $filters ) ) $atts['filters'] = json_encode( $filters );
+	return $atts;
+}
+
+add_filter( 'event_rocket_shortcode_tribeeventsminicalendarwidget_attributes', 'eventrocket_tax_atts', 10 );
+add_filter( 'event_rocket_shortcode_tribeeventsadvancedlistwidget_attributes', 'eventrocket_tax_atts', 10 );
+
+/**
  * Ensure the minimum expected attributes are available to the countdown widget.
  *
  * @param $atts
