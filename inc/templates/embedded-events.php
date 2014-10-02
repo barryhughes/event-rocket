ff9aa569fe0b424c962235ae54b7061ed3d2f2fb
barryhughes <barry@codingkills.me>
2014-10-02 08:16:20 -0700

    Echo the featured image! Fixes #8

--- inc/templates/embedded-events.php
+++ inc/templates/embedded-events.php
@@ -1,7 +1,7 @@
 <?php defined( 'ABSPATH' ) or exit() ?>
 
 <div class="eventrocket embedded-event post">
-	<div class="post-thumb"> <?php tribe_event_featured_image() ?> </div>
+	<div class="post-thumb"> <?php echo tribe_event_featured_image() ?> </div>
 	<h3> <a href="<?php the_permalink() ?>"><?php the_title() ?></a> </h3>
 	<div class="schedule"> <?php echo tribe_events_event_schedule_details() ?> </div>
 	<div class="summary"> <?php the_excerpt() ?> </div>
