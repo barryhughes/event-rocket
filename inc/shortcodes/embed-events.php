26a3192690a11d505229c036353fe48f3c2bcf3f
barryhughes <barry@codingkills.me>
2014-10-02 10:44:06 -0700

    Switch to 'list' type, 'upcoming' is effectively deprecated

--- inc/shortcodes/embed-events.php
+++ inc/shortcodes/embed-events.php
@@ -414,7 +414,7 @@ class EventRocket_EmbedEventsShortcode
 	 */
 	protected function args_display_type() {
 		$this->args['eventDisplay'] = ( isset( $this->args['start_date'] ) || isset( $this->args['end_date'] ) || isset( $this->args['post__in'] ) )
-			? 'custom' : 'upcoming';
+			? 'custom' : 'list';
 	}
 
 	/**
