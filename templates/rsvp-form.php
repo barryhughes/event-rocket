8956aad8c7af86cb430578dcbc34dc8640a4f10d
barryhughes <barry@codingkills.me>
2014-11-19 16:47:46 -0800

    Once nonce should suffice for the whole RSVP form

--- templates/rsvp-form.php
+++ templates/rsvp-form.php
@@ -8,10 +8,13 @@
 defined( 'ABSPATH' ) or exit();
 
 if ( ! $enabled ) return;
+$div_id = 'rsvp-form-' . get_the_ID();
 ?>
 
-<div class="eventrocket rsvp form">
-	<form action="" method="post">
+<div id="<?php esc_attr_e( $div_id ) ?>" class="eventrocket rsvp form">
+	<form action="#<?php esc_attr_e( $div_id ) ?>" method="post">
+		<?php wp_nonce_field( 'mark_attendance' . get_current_user_id() . get_the_ID(), 'eventrocket_rsvp_check' ) ?>
+
 		<h4> <?php _ex( 'RSVP', 'form title', 'eventrocket' ) ?> </h4>
 
 		<?php if ( $restricted && ! is_user_logged_in() ): ?>
@@ -22,24 +25,24 @@ if ( ! $enabled ) return;
 			<?php if ( $attendance->is_attending() ): ?>
 				<p>
 					<?php _e( 'You have indicated that you are attending this event!', 'eventrocket' ) ?>
-					<button class="eventrocket rsvp withdraw" name="rsvp_withdraw_<?php echo wp_create_nonce( 'withdraw' . get_the_ID() . get_current_user_id() ) ?>">
+					<button class="eventrocket rsvp withdraw" name="rsvp_withdraw">
 						<?php _e( 'Withdraw from event', 'eventrocket' ) ?>
 					</button>
 				</p>
 			<?php elseif ( $attendance->is_not_attending() ): ?>
 				<p>
 					<?php _e( 'You have indicated that you will not be attending this event!', 'eventrocket' ) ?>
-					<button class="eventrocket rsvp withdraw" name="rsvp_attend_<?php echo wp_create_nonce( 'attend' . get_the_ID() . get_current_user_id() ) ?>">
+					<button class="eventrocket rsvp attend" name="rsvp_attend">
 						<?php _e( 'Actually, I will attend!', 'eventrocket' ) ?>
 					</button>
 				</p>
 			<?php elseif ( $attendance->is_undetermined() ): ?>
 				<p>
 					<?php _e( 'Please indicate if you plan to attend this event.', 'eventrocket' ) ?>
-					<button class="eventrocket rsvp withdraw" name="rsvp_attend_<?php echo wp_create_nonce( 'attend' . get_the_ID() . get_current_user_id() ) ?>">
+					<button class="eventrocket rsvp attend" name="rsvp_attend">
 						<?php _e( 'Yes! I will attend', 'eventrocket' ) ?>
 					</button>
-					<button class="eventrocket rsvp withdraw" name="rsvp_withdraw_<?php echo wp_create_nonce( 'withdraw' . get_the_ID() . get_current_user_id() ) ?>">
+					<button class="eventrocket rsvp withdraw" name="rsvp_withdraw">
 						<?php _e( 'No, I will not', 'eventrocket' ) ?>
 					</button>
 				</p>
