ecadc5e63ebb3ae5dc7851e6d9389342a72feee3
barryhughes <barry@codingkills.me>
2015-01-26 15:30:49 -0800

    Yet more tweaks re duplication

--- admin/event-duplicator-ui.php
+++ admin/event-duplicator-ui.php
@@ -2,16 +2,17 @@
 class EventRocket_EventDuplicatorUI
 {
 	public function __construct() {
-		add_action( 'admin_enqueue_scripts', array( $this, 'add_ui_script' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'add_ui_assets' ) );
 	}
 
-	public function add_ui_script( $page_hook ) {
+	public function add_ui_assets( $page_hook ) {
 		global $post;
 		if ( 'edit.php' !== $page_hook || TribeEvents::POSTTYPE !== get_post_type( $post ) ) return;
 
-		$deps = array( 'jquery-ui-dialog', 'jquery-ui-datepicker' );
+		$deps = array( 'jquery-ui-dialog', 'jquery-ui-datepicker', 'jquery-ui-resizable' );
 		wp_enqueue_script( 'eventrocket_duplicator_ui', EVENTROCKET_URL . 'assets/duplicator.js', $deps );
 		wp_localize_script( 'eventrocket_duplicator_ui', 'eventrocket_dup', $this->js_object() );
+		wp_enqueue_style( 'eventrocket_duplicator_style', EVENTROCKET_URL . 'assets/duplicator.css' );
 	}
 
 	protected function js_object() {
