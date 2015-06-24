3346e56046050556bf249a6fd635ee3b24c9f78b
barryhughes <barry@codingkills.me>
2015-06-24 16:56:33 -0700

    Add checksum/token to ical/csv links

--- data-requests/shortcodes.php
+++ data-requests/shortcodes.php
@@ -27,13 +27,30 @@ function eventrocket_data_link( $key, $args, $content ) {
 		'event', 'from', 'to', 'category', 'categories', 'tag', 'tags', 'limit'
 	) ) );
 
+	$args['check'] = hash( 'md5', join( '|', $args ) . eventrocket_data_security_token() );
 	$args[$key] = true;
+
 	$query = http_build_query( array_map( 'urlencode', $args ) );
 	$url   = trailingslashit( home_url() ) . "?$query";
 
 	return '<a href="' . esc_url( $url ) . '" target="_blank">' . $content . '</a>';
 }
 
+/**
+ * Returns a unique-to-this-site token.
+ *
+ * @return string
+ */
+function eventrocket_data_security_token() {
+	$token = get_option( 'eventrocket_data_token', false );
+
+	if ( false === $token ) {
+		$token = hash( 'md5', date( 'YmdHisU' ) . uniqid( get_site_url() ) );
+		update_option( 'eventrocket_data_token', $token );
+	}
+
+	return $token;
+}
 
 /**
  * Example:
